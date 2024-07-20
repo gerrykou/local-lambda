02393f55b742f549aac8c297138ecd44393611a5
Gerasimos Kounadis <gerasimoskounadis@gmail.com>
2024-07-20 10:38:11 +0100

    feat: parse html from example.com

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,3 +1,9 @@
-import sys
+import requests
+from bs4 import BeautifulSoup
+
+
 def handler(event, context):
-    return 'Hello from AWS local Lambda using Python Version: ' + sys.version + '!'
+    x = requests.get('https://example.com/')
+    html = x.text
+    soup = BeautifulSoup(html, 'html.parser')
+    return f'{soup.title.string}'
