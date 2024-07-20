d2929b6336615819c34d54cca83d5a27b56f46a2
Gerasimos Kounadis <gerasimoskounadis@gmail.com>
2024-07-20 10:44:09 +0100

    fix: remove unused fstring

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,4 +6,4 @@ def handler(event, context):
     x = requests.get('https://example.com/')
     html = x.text
     soup = BeautifulSoup(html, 'html.parser')
-    return f'{soup.title.string}'
+    return soup.title.string
